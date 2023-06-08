e719ac23067198fde5a5e00a1a9182651455729a
Lucas-sarto <rulo.sarto@gmail.com>
2023-06-08 20:43:23 -0300

    segundo commit

--- ejercicio 1/ejercicio 1/Form1.Designer.vb
+++ ejercicio 1/ejercicio 1/Form1.Designer.vb
@@ -24,12 +24,15 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.cmdEjecutar = New System.Windows.Forms.Button()
         Me.lblsalida = New System.Windows.Forms.Label()
-        Me.txtdato = New System.Windows.Forms.TextBox()
+        Me.txta = New System.Windows.Forms.TextBox()
+        Me.lbla = New System.Windows.Forms.Label()
+        Me.lblb = New System.Windows.Forms.Label()
+        Me.Txtb = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'cmdEjecutar
         '
-        Me.cmdEjecutar.Location = New System.Drawing.Point(184, 148)
+        Me.cmdEjecutar.Location = New System.Drawing.Point(184, 186)
         Me.cmdEjecutar.Name = "cmdEjecutar"
         Me.cmdEjecutar.Size = New System.Drawing.Size(75, 23)
         Me.cmdEjecutar.TabIndex = 0
@@ -39,25 +42,53 @@ Partial Class Form1
         'lblsalida
         '
         Me.lblsalida.AutoSize = True
-        Me.lblsalida.Location = New System.Drawing.Point(306, 81)
+        Me.lblsalida.Location = New System.Drawing.Point(300, 97)
         Me.lblsalida.Name = "lblsalida"
         Me.lblsalida.Size = New System.Drawing.Size(39, 13)
         Me.lblsalida.TabIndex = 1
         Me.lblsalida.Text = "Label1"
         '
-        'txtdato
+        'txta
         '
-        Me.txtdato.Location = New System.Drawing.Point(68, 81)
-        Me.txtdato.Name = "txtdato"
-        Me.txtdato.Size = New System.Drawing.Size(100, 20)
-        Me.txtdato.TabIndex = 2
+        Me.txta.Location = New System.Drawing.Point(66, 59)
+        Me.txta.Name = "txta"
+        Me.txta.Size = New System.Drawing.Size(100, 20)
+        Me.txta.TabIndex = 2
+        '
+        'lbla
+        '
+        Me.lbla.AutoSize = True
+        Me.lbla.Location = New System.Drawing.Point(66, 40)
+        Me.lbla.Name = "lbla"
+        Me.lbla.Size = New System.Drawing.Size(103, 13)
+        Me.lbla.TabIndex = 3
+        Me.lbla.Text = "Ingrese el valor de a"
+        '
+        'lblb
+        '
+        Me.lblb.AutoSize = True
+        Me.lblb.Location = New System.Drawing.Point(66, 97)
+        Me.lblb.Name = "lblb"
+        Me.lblb.Size = New System.Drawing.Size(103, 13)
+        Me.lblb.TabIndex = 4
+        Me.lblb.Text = "Ingrese el valor de b"
+        '
+        'Txtb
+        '
+        Me.Txtb.Location = New System.Drawing.Point(69, 131)
+        Me.Txtb.Name = "Txtb"
+        Me.Txtb.Size = New System.Drawing.Size(100, 20)
+        Me.Txtb.TabIndex = 5
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(412, 261)
-        Me.Controls.Add(Me.txtdato)
+        Me.Controls.Add(Me.Txtb)
+        Me.Controls.Add(Me.lblb)
+        Me.Controls.Add(Me.lbla)
+        Me.Controls.Add(Me.txta)
         Me.Controls.Add(Me.lblsalida)
         Me.Controls.Add(Me.cmdEjecutar)
         Me.Name = "Form1"
@@ -68,6 +99,9 @@ Partial Class Form1
     End Sub
     Friend WithEvents cmdEjecutar As System.Windows.Forms.Button
     Friend WithEvents lblsalida As System.Windows.Forms.Label
-    Friend WithEvents txtdato As System.Windows.Forms.TextBox
+    Friend WithEvents txta As System.Windows.Forms.TextBox
+    Friend WithEvents lbla As System.Windows.Forms.Label
+    Friend WithEvents lblb As System.Windows.Forms.Label
+    Friend WithEvents Txtb As System.Windows.Forms.TextBox
 
 End Class
--- ejercicio 1/ejercicio 1/Form1.vb
+++ ejercicio 1/ejercicio 1/Form1.vb
@@ -1,3 +1,11 @@
 ﻿Public Class Form1
 
+    Private Sub cmdEjecutar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdEjecutar.Click
+        Dim a As Integer
+        Dim b As Integer
+        a = txta.Text
+        b = Txtb.Text
+
+        
+    End Sub
 End Class
